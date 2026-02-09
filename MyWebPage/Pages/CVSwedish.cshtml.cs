ec0d0f2246daf977b61bc79b8e45f61b2106825b
Jerome Gordon-Bock <jerome.bock@gmail.com>
2026-02-09 13:54:58 +0100

    Add project files.

---
+++ MyWebPage/Pages/CVSwedish.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyWebPage.Pages
+{
+    public class CVSwedishModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
