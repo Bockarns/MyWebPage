751b0ebd52a69ef38f8f53e73956526d49f05ee7
Jerome Gordon-Bock <jerome.bock@gmail.com>
2026-02-10 17:00:58 +0100

    Added memory game

---
+++ MyWebPage/Pages/memory.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyWebPage.Pages
+{
+    public class memoryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
